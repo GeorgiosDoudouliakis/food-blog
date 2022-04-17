a3e2205cf1df6784a3e72e7d45522c08933b509a
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-04-17 22:34:46 +0300

    feat: Footer layout and styling and website main html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from '@shared/shared.module';
 import { HeaderModule } from '@shared/modules/header/header.module';
+import { FooterModule } from '@shared/modules/footer/footer.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HeaderModule
+    HeaderModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
