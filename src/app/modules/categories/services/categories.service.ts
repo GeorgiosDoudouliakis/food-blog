fb76fa944928cc19ec2fad311b85db5fd7857e6f
gdoudouliakis <g.doudouliakis@schoox.com>
2022-04-23 15:48:20 +0300

    refactor: Improve getCategories http call combined with loader

--- src/app/modules/categories/services/categories.service.ts
+++ src/app/modules/categories/services/categories.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { finalize, pluck, shareReplay } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { concatMap, finalize, pluck, shareReplay, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { Categories, Category } from '../models/categories.model';
 import { LoaderService } from '@shared/services/loader/loader.service';
 
@@ -14,12 +14,12 @@ export class CategoriesService {
   ) { }
 
   getCategories(): Observable<Category[]> {
-    this.loaderService.loadingState(true);
-    return this.http.get<Categories>('https://www.themealdb.com/api/json/v1/1/categories.php')
-                .pipe(
-                  pluck('categories'),
-                  finalize(() => this.loaderService.loadingState(false)),
-                  shareReplay()
-                );
+    return of(null).pipe(
+      tap(() => this.loaderService.loadingState(true)),
+      concatMap(() => this.http.get<Categories>('https://www.themealdb.com/api/json/v1/1/categories.php')),
+      pluck('categories'),
+      finalize(() => this.loaderService.loadingState(false)),
+      shareReplay()
+    )
   }
 }
